0e74778e3fa1b6431b0887fa9e886f9c85f8bea4
Changhwan Lee <chlee1252@gmail.com>
2021-01-22 17:21:20 +0900

    feat: initialize vue store

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,19 @@
 <template>
-  <div>Hello</div>
+  <div class="container">
+    <div class="item"></div>
+  </div>
 </template>
+
+<style lang="scss">
+  @import "./scss/main";
+
+  .container {
+    display: flex;
+    .item {
+      width: 100px;
+      height: 100px;
+      background-color: red;
+    }
+  }
+</style>
 
