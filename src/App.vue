4ecc5ea178f8a9292266f88abf140e39fa289029
Changhwan Lee <chlee1252@gmail.com>
2021-01-26 21:58:56 +0900

    feat: navigation bar and swiper

--- src/App.vue
+++ src/App.vue
@@ -2,17 +2,20 @@
   <div>
     <l-n-b></l-n-b>
     <my-header></my-header>
+    <billboard></billboard>
   </div>
 </template>
 
 <script>
 import LNB from "./components/LNB";
 import MyHeader from "./components/MyHeader";
+import Billboard from "./components/Billboard";
 
 export default {
   components: {
     LNB,
-    MyHeader
+    MyHeader,
+    Billboard,
   }
 }
 </script>
